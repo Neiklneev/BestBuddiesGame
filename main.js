dcbbeccd4af4b16033f9d22d29b3d146a3fed4c3
Atharv Agrawal <neikln33v@gmail.com>
2025-07-30 01:49:38 +0400

    Added new font, single level changer, sound toggle, and better speed change. Cleaned up code a lot

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
 // Importing our game engine, Kaplay
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 
-const speedArray = [60, 80, 120, 150, 180];
-const bpsArray = [0.3, 0.7, 1.2, 2, 3];
+const speedArray = [60, 80, 100, 115, 130, 140, 150, 155, 160, 180]
+const bpsArray = [0.3, 0.5, 0.8, 1.2, 1.6, 1.9, 2.2, 2.4, 2.5, 3.0]
 
+let sound = 1;
+let speed = 10;
 
 kaplay({
-  background:"#87CEEB"
+  background:"#87CEEB",
+  font: "tahoma"
 });
 
-scene("main", ({speed, bps}) => {
+
+loadFont("tahoma", "assets/tahoma.ttf")
+
+scene("main", () => {
   loadSound("pop", "assets/pop.mp3")
   loadSound("bg", "assets/background.mp3")
+  loadSprite("cloud", "assets/cloud.png")
 
-  play("bg", {
-    loop:false,
-    volume: 0.2
+
+  for (let x of [1, 2, 3, 4, 5]) {
+    add([
+      sprite("cloud"),
+      pos(rand(vec2(width(), height()))),
+      scale(0.5)
+    ])
+  }
+
+  let bg = play("bg", {
+    loop:true,
+    volume: 0.2*sound
   })
+
+  let settingsButton = add([
+    text("⚙"),
+    color(0, 0, 0),
+    pos(width()-20, 20),
+    area(),
+    anchor("topright")
+  ])
 
   let scoreCounter = add([
       text("0"),
+      color(0, 0, 0),
       pos(20, 20),
       anchor("topleft")
     ])
@@ -27,20 +52,19 @@ scene("main", ({speed, bps}) => {
   let score = 0;
 
 
-  loop(1/bps, () => {
+  loop(1/bpsArray[speed-1], () => {
 
-    console.log(`POINT 2 REACHED, speed ${speed}`)
-    let position = rand(vec2(110, height()-80), vec2(width()-270, height() - 170))
+    let position = rand(vec2(60, height()), vec2(width()-60, height() - 170))
 
     let circleSprite = add([
 
       circle(60),
       area(),
       pos(position),
-      color(200, 0, 0),
+      color(rand(rgb(255, 255, 255))),
       outline(4),
       anchor("center"),
-      move(UP, speed),
+      move(UP, rand(speedArray[speed-1]-15, speedArray[speed-1]+15)),
       "balloon",
 
 
@@ -52,7 +76,7 @@ scene("main", ({speed, bps}) => {
       addKaboom(circleSprite.pos);
       shake();
       play("pop", {
-        volume:0.5,
+        volume:0.5*sound,
         loop: false
       })
       circleSprite.destroy();
@@ -65,52 +89,104 @@ scene("main", ({speed, bps}) => {
   onUpdate(() => {
     for (let balloon of get("balloon")) {
       if (balloon.pos.y < 0) {
+        bg.stop();
         go("over", score);
       }
+    }
+
+    if (settingsButton.isHovering()) {
+      settingsButton.color = Color.fromHex(0x555555);
+    } else {
+      settingsButton.color = BLACK;
     }
     scoreCounter.text = score;
   });
 
-  onKeyPress("space", () => {
-    go("settings", {speed:speed, bps:bps})
+  settingsButton.onClick(() => {
+    bg.stop();
+    go("settings")
   })
 });
 
 
-scene("settings", ({speed, bps}) => {
+scene("settings", () => {
 
-  let speedCounter = add([ text(speedArray.indexOf(speed)+1, { size:60 }), color(0, 0, 0), pos(width()/2, height()/3) ]);
-  let bpsCounter = add([ text(bpsArray.indexOf(bps)+1, { size:60 }), color(0, 0, 0), pos(width()/2, 2 * height()/3) ]);
+  loadSprite("balloons", "assets/balloons.png")
+  loadSprite("speed", "assets/speed.png")
+  loadSprite("musicOn", "assets/musicOn.png")
+  loadSprite("musicNo", "assets/musicNo.png")
+  add([
+    text("Settings", {size:60}),
+    pos(width()/2, 100),
+    color(0, 0, 0),
+    anchor("center")
+  ])
 
+  let speedCounter = add([
+    text(speed, { size:60 }),
+    color(0, 0, 0),
+    pos(width()/2, height()/2-40),
+    anchor("center")
+  ]);
 
-  onKeyPress("a", () => {
-    if (Number(speedCounter.text) > 1) {
-      let currentSpeed = Number(speedCounter.text) - 1; 
-      speedCounter.text=currentSpeed}
+    let music = add([ sprite(sound ? "musicOn" : "musicNo"), pos(width()/2, height()/2 + 50), anchor("center"), scale(0.2), area(), "music"])
+
+  let speedUp = add([ text("►", { size:60 }), area(), color(255, 255, 255), pos(width()/2 + 70, height()/2-40), anchor("center")])
+  let speedDown = add([ text("◄", { size:60 }), area(), color(255, 255, 255),pos(width()/2 - 70, height()/2-40), anchor("center")])
+  speedDown.onClick(() => {
+    if (speed > 1) {
+      speed -= 1
+      speedCounter.text = speed
+    }
   });
 
-  onKeyPress("d", () => {
-    if (Number(speedCounter.text) < 5) {
-      let currentSpeed = Number(speedCounter.text) + 1; 
-      speedCounter.text=currentSpeed}
+  speedUp.onClick(() => {
+    if (speed < 10) {
+      speed += 1
+      speedCounter.text = speed
+    }
   });
 
-  onKeyPress("w", () => {
-    if (Number(bpsCounter.text) < 5) {
-      let currentBps = Number(bpsCounter.text) + 1; 
-      bpsCounter.text=currentBps}
-  });
+  onClick("music", () => {
+    sound = !sound;
+    console.log("clicked, sound changing");
+    loadSprite("musicOn", "assets/musicOn.png")
+    loadSprite("musicNo", "assets/musicNo.png");
+    destroyAll("music");
+    var music = add([ sprite(sound ? "musicOn" : "musicNo"), pos(width()/2, height()/2 + 50), anchor("center"), scale(0.2), area(), "music"])
+  })
 
-  onKeyPress("s", () => {
-    if (Number(bpsCounter.text) > 1) {
-      let currentBps = Number(bpsCounter.text) - 1; 
-      bpsCounter.text=currentBps}
-  });
+  let saveButton = add([
+    rect(200+width()/4, 100, {radius:20}),
+    color("#00FF00"),
+    pos(width()/2, height() - 100),
+    anchor("center"),
+    outline(4),
+    scale(1),
+    area()
+  ]);
 
-  onMousePress(() => {
-    go("main", {speed:speedArray[speedCounter.text - 1], bps:bpsArray[bpsCounter.text-1]});
-    console.log(speedArray[Number(speedCounter.text) - 1])
-    console.log(bpsArray[Number(bpsCounter.text) - 1])
+  saveButton.onClick(() => {
+    go("main");
+  })
+
+  onUpdate(() => {
+    for (let button of get("button")) {
+      if (button.isHovering()) {
+        button.color = Color.fromHex(0x888888);
+      } else {
+        button.color = Color.fromHex(0xFFFFFF);
+      }
+    }
+
+    if (saveButton.isHovering()) {
+      saveButton.scale.x += (((1.2-saveButton.scale.x)/8))
+      saveButton.scale.y += (((1.2-saveButton.scale.y)/8))
+    } else {
+      saveButton.scale.x += ((1-saveButton.scale.x)/8)
+      saveButton.scale.y += ((1-saveButton.scale.y)/8)
+    }
+
   })
 })
 
@@ -118,21 +194,27 @@ scene("settings", ({speed, bps}) => {
 
 scene("over", (finalScore) => {
   add([
-    text(`Game Over - You Got ${finalScore} Points!`, { "size": 60 }),
+    text("Game Over", { "size": 60 }),
     color(255, 0, 0),
-    pos(width()/2, 100),
+    pos(width()/2, 120),
+    anchor("center")
+  ]);
+  add([
+    text(finalScore, { "size": 40 }),
+    color(0, 0, 0),
+    pos(width()/2, 50),
     anchor("center")
   ]);
 
   add([
-    text("Click The Button Above To Play Again!", {"size": 40}),
+    text("Click To Play!", {"size": 40}),
     color(0, 0, 0),
     pos(width()/2, height() - 60),
     anchor("center")
   ])
 
   let backButton = add([
-    rect(400, 100, {radius:20}),
+    rect(200+width()/4, 100, {radius:20}),
     color("#00FF00"),
     pos(center()),
     anchor("center"),
@@ -143,8 +225,8 @@ scene("over", (finalScore) => {
 
   onUpdate(() => {
     if (backButton.isHovering()) {
-      backButton.scale.x += (((1.1-backButton.scale.x)/8))
-      backButton.scale.y += (((1.1-backButton.scale.y)/8))
+      backButton.scale.x += (((1.2-backButton.scale.x)/8))
+      backButton.scale.y += (((1.2-backButton.scale.y)/8))
     } else {
       backButton.scale.x += ((1-backButton.scale.x)/8)
       backButton.scale.y += ((1-backButton.scale.y)/8)
@@ -152,9 +234,9 @@ scene("over", (finalScore) => {
   });
 
   backButton.onClick(() => {
-    go("main", {speed: 180, bps: 3});
+    go("main");
   })
 
 })
 // Activating the main scene
-go("main", {speed: 180, bps: 3});
+go("main");
